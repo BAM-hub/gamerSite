c4d5a74f35863ce7c7d9ca00c87866bd122bd0cf
BAM-hub <bsharamin12@gmail.com>
2021-10-23 00:43:10 +0300

    starting the socket server along

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const { Socket } = require('socket.io-client');
 const connectDB = require('./config/db');
-
+const startSocket = require('./socket/server');
 const app = express();
 
 //connect db
@@ -15,10 +16,10 @@ app.get('/', (req, res) => res.send('API running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => { 
     console.log(`server started on ${PORT}`);
 });
 
+startSocket();
