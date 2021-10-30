e72025c99f322bf0608a26bc53a92267c1bb86dc
BAM-hub <bsharamin12@gmail.com>
2021-10-30 13:19:28 +0200

    auth reducer handel cases and state

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,41 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS
+} from '../actions/types';
+
+const initialState = { 
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+  
+  switch(type) {
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case REGISTER_FAIL:
+    case LOGIN_FAIL:
+      localStorage.removeItem('tokent');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
+    default:
+      return state;
+  };
+};
