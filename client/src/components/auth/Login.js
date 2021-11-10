a35dce06e8c34aca004e6e44c32350ef1e609073
BAM-hub <bsharamin12@gmail.com>
2021-11-10 22:38:53 +0200

    checks for the token if valid no need to login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { setAlert } from '../../actions/alert';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { login } from '../../actions/auth';
+import { login, loggedIn } from '../../actions/auth';
 
 
-const Login = ({ setAlert, login }) => {
+
+const Login = ({ 
+  auth:{ isAuthenticated },
+  setAlert,
+  login,
+  loggedIn
+ }) => {
+
+  useEffect(()=>{
+    loggedIn()
+  },[loggedIn]);
+
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -18,14 +30,19 @@ const Login = ({ setAlert, login }) => {
     [e.target.name]: e.target.value
   });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if(email === '' || password === '') {
       console.log('fail');
       return;
-    }
-    login({ email, password });
+    };
+
+    await login({ email, password });
   };
+
+  if(isAuthenticated) {
+    return <Redirect to="/profile" />;
+  }
 
   return (
     <form onSubmit={e => onSubmit(e)} id='form'>
@@ -46,12 +63,18 @@ const Login = ({ setAlert, login }) => {
   );
 }
 
-Login.prototypes = ({
+Login.prototypes = {
   setAlert: PropTypes.func.isRequired,
   login: PropTypes.func.isRequired,
+  loggedIn: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStatetoProps = state => ({
+ auth: state.auth
 });
 
 export default connect(
-  null,
-  { setAlert, login }
+  mapStatetoProps,
+  { setAlert, login, loggedIn }
 )(Login);
