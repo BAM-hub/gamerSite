aa8d75c5c473658b4fa1c4b007661f07822f1456
BAM-hub <bsharamin12@gmail.com>
2021-11-13 02:40:43 +0200

    the profile is fetched on load

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,9 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import UserInfo from './UserInfo';
 import ProfileContent from './ProfileContent';
 import ChatOverview from './ChatOverview';
 
-const Profile = () => {
+import { getProfile } from '../../actions/profile';
+
+const Profile = ({ getProfile, email }) => {
+  useEffect(()=>{
+    getProfile(email);
+  },[getProfile, email]);
   return (
     <Fragment>
       <UserInfo />
@@ -13,4 +20,18 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+
+Profile.propTypes = {
+  auth: PropTypes.object.isRequired,
+  getProfile: PropTypes.func.isRequired,
+}
+
+
+const mapStateToProps = state => ({
+  email: state.auth.email
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfile }
+)(Profile);
