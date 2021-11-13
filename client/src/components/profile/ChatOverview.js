13e6f5a9c82638806e635ef5478bc72fcd239b9a
BAM-hub <bsharamin12@gmail.com>
2021-11-13 02:40:17 +0200

    classname alert fixed

--- client/src/components/profile/ChatOverview.js
+++ client/src/components/profile/ChatOverview.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 const ChatOverview = () => {
   return (
-    <div class="right">
+    <div className="right">
 
-    <form action="" method="post" class="search-bar">
+    <form action="" method="post" className="search-bar">
       <input type="text" placeholder="search chat" />
       <button type="submit">
-        <i class="fas fa-paper-plane"></i>
+        <i className="fas fa-paper-plane"></i>
       </button>
     </form>
 
     <h3>Chat Rooms</h3>
 
-    <div class="chat-room">
-      <div class="room-name">
+    <div className="chat-room">
+      <div className="room-name">
         <p>BOTW</p>
       </div>
 
-      <div class="last-message">
+      <div className="last-message">
         <p>hey man i ju...</p>
       </div>
     </div>
