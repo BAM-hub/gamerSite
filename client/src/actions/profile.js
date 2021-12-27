291fd66101d2a4c4d52cb29eccf3c0b9673e76f7
BAM-hub <bsharamin12@gmail.com>
2021-12-28 00:27:59 +0200

    changed the requests based on the new object structure

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -104,7 +104,8 @@ export const createProfile = (formData, token, email) => async dispatch => {
   }
 };
 
-export const editGameList = (gameList, images, email, token) => async dispatch => {
+export const editGameList = (gameList, email, token) => async dispatch => {
+
   try {
     let config = {
       headers: {
@@ -112,36 +113,56 @@ export const editGameList = (gameList, images, email, token) => async dispatch =
         'x-auth-token': token
       }
     };
+    let newGamlist = gameList.map(game => {
+      let {name, score, tags} = game;
+      return {name, score, tags};
+    });
 
-    let body = JSON.stringify({ gameList, email });
 
+    let body = JSON.stringify({ 'gameList': newGamlist, email });
     let res = await axios.post('api/games/add-game-to-profile', body, config);
 
     let existingGames = res.data.existingGames[0];
-    
+
     config = {
       headers: {
         'content-type': 'multipart/encrypted'
       }
     };
 
+
     existingGames.map(async (game ,i) => {
-      if(!game) return;
-
-      body = new FormData();
-      body.append('file', images[i], images[i].name);
-
-      await axios.post(`api/profile/upload/${email}/Games:${gameList[i].name}`, body, config);
-
-      if(i === existingGames.length -1) {
-        config = {
+      
+      if(i === existingGames.length -1 && !game) {
+        let config = {
           headers: {
             'Content-Type': 'application/json',
             'x-auth-token': token
           }
         };
+        let res = await axios.get(`api/games/game-list/${email}`, config);
+          dispatch({
+            type: GAME_LIST_UPDATED,
+            payload: res.data
+          });
+      }
+
+      if(!game) return;
+      console.log(gameList[i].image , gameList[i].image.name);
+
+      body = new FormData();
+      body.append('file', gameList[i].image , gameList[i].image.name);
+
+      await axios.post(`api/profile/upload/${email}/Games:${gameList[i].image.name}`, body, config);
       
-          res = await axios.get(`api/games/game-list/${email}`, config);
+      if(i === existingGames.length -1) {
+        let config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-auth-token': token
+          }
+        };
+        let res = await axios.get(`api/games/game-list/${email}`, config);
           dispatch({
             type: GAME_LIST_UPDATED,
             payload: res.data
