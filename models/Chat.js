61417c3f386cd7e55745f5ffba091d4de60a2cee
BAM-hub <bsharamin12@gmail.com>
2022-01-15 01:23:46 +0200

    changed the chat schema

--- models/Chat.js
+++ models/Chat.js
@@ -2,27 +2,33 @@ const mongoose = require('mongoose');
 
 const ChatSchema = mongoose.schema({
   conversation: [{
-    sender: {
-      type: String,
-      required: true
-    },
-    text: {
-      type: String,
-      required: true
-    },
-    conversationId: {
-      type: String,
-      required: true
-    },
-    time: {
-      type: Date,
-      default: Date.now
-    },
-    admin: {
-      type: String,
-      required: true
+    message: {
+      sender: {
+        type: String,
+        required: true
+      },
+      recipient: {
+        type: String,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      time: {
+        type: Date,
+        default: Date.now
+      },
+      admin: {
+        type: String,
+        required: true
+      }
     }
-  }]
+  }],
+  conversationId: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = Chat = mongoose.model('Chat', chatScheme);
