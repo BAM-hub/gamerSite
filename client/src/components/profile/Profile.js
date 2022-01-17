85d75bf424445e65d6e54f519df01d8a73a88122
BAM-hub <bsharamin12@gmail.com>
2022-01-17 03:16:47 +0200

    socket lifecycle managment (might be modified)

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,16 +1,39 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import UserInfo from './UserInfo';
 import ProfileContent from './ProfileContent';
 import ChatOverview from './ChatOverview';
+import { io } from 'socket.io-client';
+import { 
+  SOCKET_CONNECTION,
+  SOCKET_CLOSE
+} from '../../actions/types';
 
 import { getProfile } from '../../actions/profile';
 
 const Profile = ({ getProfile, auth:{email} }) => {
-  useEffect(()=>{
+  const dispatch = useDispatch();
+  useEffect(() => {
     getProfile(email);
-  },[getProfile, email]);
+    const socket = io('localhost:8000');
+    dispatch({
+      type: SOCKET_CONNECTION,
+      payload: socket
+    });
+
+    return () => {
+      socket.off('connection');
+      //socket.close();
+      //socket.off();
+      dispatch({
+        type: SOCKET_CLOSE,
+        payload: io('localhost:8000')
+      });
+      socket.close();
+    }
+  }, [getProfile, email, dispatch]);
+
   return (
     <Fragment>
       <UserInfo />
