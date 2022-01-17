1fca7163b8f168127ed6c685eff2f7a9a0490d75
BAM-hub <bsharamin12@gmail.com>
2022-01-17 03:14:21 +0200

    chat component is initially working

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,54 +1,75 @@
 import PropTypes from 'prop-types';
-import React, {
-    useEffect,
-    useRef,
-    useState } from 'react';
-import  io  from "socket.io-client";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
-const socket = io('http://localhost:8000');
-
-const Chat = () => {
-  const [roomName, setRoomName] = useState();
+const Chat = ({auth:{ name, user, socket, email }}) => {
+  const [roomName, setRoomName] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const txt = useRef();
+  const [recipient, setRecipient] = useState('');
 
+  //get room name
   useEffect(() => {
-    socket.emit('connection');
-    socket.emit('join');
-    socket.on('roomName', (room) => {
-      setRoomName(room); 
+    socket.emit('join', user);
+    return () => {
+      //socket.close();
+      socket.off('join');
+      socket.off('connection');
+      socket.close();
+    }
+  }, [user, socket, email]);
+  socket.on('roomName', room => setRoomName(room));
+
+  //logic
+  const send = (e) => {
+    e.preventDefault();
+    const msg = { message, name, id: uuidv4(), reciver: recipient };
+    socket.emit('send_message', msg);
+  }
+  useEffect(() => {
+    socket.on('recive_message', (msg) => {
+    setMessages(
+      prevState => [...prevState, msg] 
+    );
     });
-  }, []);
-
-  function sendMessage() {
-   socket.emit('message', message);
-   setMessages([...messages, message]);
-   console.log(`${message} sent`);
-  };
-
-  socket.on('message', (message) =>{
-    setMessages([...messages, message]);
+    return () => {
+      socket.off('recive_message');
+    }
   });
   return (
-  <>
-    {roomName}
-    <div>
-    <input
-      ref={txt}
-      value={message}
-      onChange={e => setMessage(e.target.value)}
-    />
-    <button onClick={e => sendMessage()}>send</button>
-    </div>
+    <>
+      <div className="room-name">{roomName}</div>
+      <div className="message-send">
+        <input 
+          type="text" 
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={(e) => send(e)}>send</button>
+      </div>
 
-    {messages.map(msg =>(
-      <div>{msg}</div>
-    ))}
-  </>
+      <div className="messages">
+        {
+          messages.map(msg => (
+            <div className="message">{msg.message}</div>
+          ))
+        }
+      </div>
+    </>
   );
 };
 
-Chat.propTypes = {};
+const mapStatetoProps = state => ({
+  auth: state.auth
+});
 
-export default Chat;
+Chat.propTypes = {
+  auth: PropTypes.object,
+};
+
+
+export default connect(
+  mapStatetoProps
+)(Chat);
