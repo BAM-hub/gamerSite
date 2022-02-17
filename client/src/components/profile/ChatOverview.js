11db08ff7c907fafbd343d7b7eb4e49ac802b6b5
BAM-hub <bsharamin12@gmail.com>
2022-02-17 02:36:21 +0200

    chat overview is now dynamic but still not done

--- client/src/components/profile/ChatOverview.js
+++ client/src/components/profile/ChatOverview.js
@@ -1,30 +1,39 @@
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { getUserByEmail } from '../../actions/chat';
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import { getUserByEmail, clearSearch, selectChat } from '../../actions/chat';
+import { Redirect } from 'react-router-dom';
 
 const ChatOverview = ({ 
   auth: { socket, user },
   getUserByEmail,
-  chat 
+  chat,
+  conversations,
+  clearSearch,
+  selectChat
 }) => {
   const [email, setEmail] = useState('');
   const [redirect, setRedirect] = useState(false);
+  const [selectedChat, setSelectedChat] = useState('');
 
 
-  const toChat = () => {
-
+  const toChat = (id) => {
+    setSelectedChat(id);
+    clearSearch();
+    selectChat(id);
+    setRedirect(true);
   };
 
   const searchChat = (e) => {
     e.preventDefault();
     getUserByEmail(email);
     if(chat.search._id === null) return;
+    setSelectedChat(user + ' ' + chat.search._id);
+    selectChat(user + ' ' + chat.search._id);
     setRedirect(true);
   }
 
-  if(redirect) return <Redirect to='/chat' />
+  if(redirect) return <Redirect to={`/chat/${selectedChat}`} />
  
   return (
     <div className="right">
@@ -42,16 +51,23 @@ const ChatOverview = ({
     </form>
 
     <h3>Chat Rooms</h3>
+    <div className="chat-container">
+      {conversations.map(convo => (
+      <div 
+        className="chat-room" key={convo.conversationId} 
+        onClick={() => toChat(convo.conversationId)} 
+      >
+        <div className="room-name">
+          <p>{convo.recipient}</p>
+        </div>
 
-    <div className="chat-room">
-      <div className="room-name" onClick={() => toChat()}>
-        <p>BOTW</p>
+        <div className="last-message">
+          <p>hey man i ju...</p>
+        </div>
       </div>
-
-      <div className="last-message">
-        <p>hey man i ju...</p>
-      </div>
+      ))}
     </div>
+
   </div>
   );
 };
@@ -59,15 +75,19 @@ const ChatOverview = ({
 ChatOverview.propTypes = {
   auth: PropTypes.object.isRequired,
   chat: PropTypes.object.isRequired,
-  getUserByEmail: PropTypes.func.isRequired
+  conversations: PropTypes.array.isRequired,
+  getUserByEmail: PropTypes.func.isRequired,
+  clearSearch: PropTypes.func.isRequired,
+  selectChat: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
   auth: state.auth,
-  chat: state.chat
+  chat: state.chat,
+  conversations: state.profile.conversations
 });
 
 export default connect(
   mapStateToProps,
-  { getUserByEmail }
+  { getUserByEmail, clearSearch, selectChat }
 )(ChatOverview);
