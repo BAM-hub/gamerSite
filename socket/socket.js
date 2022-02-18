6d69b28816ce2b093abbb63f833dc262a00f20ab
BAM-hub <bsharamin12@gmail.com>
2022-02-18 02:35:24 +0200

    wp

--- socket/socket.js
+++ socket/socket.js
@@ -1,11 +1,10 @@
-const room = 'test room';
 
 const socketHandler = (socket) => {
     console.log(`connection by ${socket.id}`);
 
     socket.on('join', (id) => {
       socket.join(id);
-      socket.emit('roomName', 'test room');
+      console.log(`${id} joined`);
       //console.log(io.engine.clientsCount);
       //console.log(`${id} joined on ${socket.id}`)
    });
@@ -16,7 +15,7 @@ const socketHandler = (socket) => {
       // console.log(io.engine.clientsCount)
       // console.log(io.of("/").adapter.sids);
       // console.log(socket.id);
-      socket.to('61ba565477dddaa3ce37b9c0').emit('recive_message', msg);
+      socket.to(msg.reciver).emit('recive_message', msg);
     });
   
     socket.on('disconnect', () => {
