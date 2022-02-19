61f4cc05e471ccba74147fd9fbc747c542122eab
BAM-hub <bsharamin12@gmail.com>
2022-02-19 04:23:18 +0200

    more chat actions are added

--- client/src/actions/chat.js
+++ client/src/actions/chat.js
@@ -3,11 +3,54 @@ import {
   USER_FOUND,
   USER_NOT_FOUND,
   CLEAR_SEARCH,
-  SET_SELECTED_CHAT
+  SET_SELECTED_CHAT,
+  CONVERSATION_FOUND,
+  CONVERSATION_NOT_FOUND,
+  CONVERSAITION_CREATED,
+  CONVERSATION_CREATE_FAIL
 } from './types';
 
-export const findChat = (email) => async dispatch => {
-  
+
+export const createChat = (creator, recipient) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'x-auth-token': localStorage.getItem('token'),
+        'Content-Type': 'application/json'
+      }
+    }
+    const body = JSON.stringify({ creator, recipient });
+    const res = await axios.post('/api/chat/create-conversation', body, config);
+    dispatch({
+      type: CONVERSAITION_CREATED,
+      payload: res.data
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: CONVERSATION_CREATE_FAIL
+    });
+  }
+}
+
+export const findChat = id => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    const res = await axios.get(`/api/chat/${id}`, config);
+    dispatch({
+      type: CONVERSATION_FOUND,
+      payload: res.data
+    })
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: CONVERSATION_NOT_FOUND
+    });
+  }
 }
 
 export const getUserByEmail = (email) => async dispatch => {
--- client/src/reducers/chat.js
+++ client/src/reducers/chat.js
@@ -2,7 +2,9 @@ import {
   USER_FOUND,
   USER_NOT_FOUND,
   CLEAR_SEARCH,
-  SET_SELECTED_CHAT
+  SET_SELECTED_CHAT,
+  CONVERSATION_FOUND,
+  CONVERSAITION_CREATED
 } from '../actions/types';
 
 const initialState = {
@@ -14,7 +16,8 @@ const initialState = {
   },
   selectedChat: {
     _id: '',
-  }
+  },
+  chats: []
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -22,12 +25,28 @@ export default function(state = initialState, action) {
   const  { type, payload } = action;
   switch(type) {
     case USER_FOUND:
-      return { search: { ...payload }, ...state };
+      return { ...state, search: {...payload} };
     case SET_SELECTED_CHAT:
       return { ...state, selectedChat: { _id: payload } }
+    case CONVERSATION_FOUND:
+      return {
+        ...state,
+        chats: [...state.chats, payload]
+      }
+    case CONVERSAITION_CREATED:
+      const { conversationId } = payload;
+      return {
+        ...state,
+        chats: [
+          ...state.chats, {
+          conversationId: conversationId,
+          conversation: []
+        }]
+      }  
     case USER_NOT_FOUND:
     case CLEAR_SEARCH:
-        return { 
+        return {
+          ...state, 
           search: {
             _id: null,
             name: '',
