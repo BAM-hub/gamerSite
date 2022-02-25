e307e90022ed583db029d79ee66ada1b69eb4301
BAM-hub <bsharamin12@gmail.com>
2022-02-25 04:50:52 +0300

    new chat and message saving handeld on the socket

--- socket/socket.js
+++ socket/socket.js
@@ -1,23 +1,38 @@
+const Profile = require('../models/Profile');
+const Chat = require('../models/Chat');
 
-const socketHandler = (socket) => {
+const socketHandler = async (socket) => {
     console.log(`connection by ${socket.id}`);
-
     socket.on('join', (id) => {
       socket.join(id);
       console.log(`${id} joined`);
-      //console.log(io.engine.clientsCount);
-      //console.log(`${id} joined on ${socket.id}`)
    });
   
-  
-    socket.on('send_message', msg => {
+    socket.on('send_message', async msg => {
       console.log(msg);
-      // console.log(io.engine.clientsCount)
-      // console.log(io.of("/").adapter.sids);
-      // console.log(socket.id);
+      await Chat.findOneAndUpdate(
+        {conversationId: msg.chatId},
+        {
+          $addToSet: {
+            conversation: [
+              {
+                sender: msg.sender,
+                reciver: msg.reciver,
+                id: msg.id,
+                message: msg.message,
+                time: msg.time
+            }
+            ]
+          }
+        }
+      );
       socket.to(msg.reciver).emit('recive_message', msg);
     });
-  
+
+    socket.on('new-chat', (data) => {
+      socket.to(data.recipient).emit('new-chat', data);
+    });
+
     socket.on('disconnect', () => {
       console.log('disconnect');
     });  
