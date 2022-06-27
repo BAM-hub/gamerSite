f7fcfe6ee2534b789d7367fb7a1631728f39c1ff
BAM-hub <bsharamin12@gmail.com>
2022-06-27 06:22:03 +0300

    reformated with prettier

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -8,22 +8,22 @@ import {
   UPLOAD_IMAGE_FAILED,
   IMAGE_DELETED,
   IMAGE_DELETE_FAIL,
-  GAME_LIST_UPDATED
-} from './types';
-import FormData from 'form-data'
+  GAME_LIST_UPDATED,
+} from "./types";
+import FormData from "form-data";
 
-export const getProfile = (email) => async dispatch => {
+export const getProfile = (email) => async (dispatch) => {
   try {
     let config = {
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     };
 
     let res = await axios.get(`/api/profile/${email}`, config);
     dispatch({
       type: PROFILE_FOUND,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     console.log(err);
@@ -31,153 +31,152 @@ export const getProfile = (email) => async dispatch => {
       type: PROFILE_NOT_FOUND,
     });
   }
-
 };
 
-export const uploadImage = (file, email, image) => async dispatch => {
+export const uploadImage = (file, email, image) => async (dispatch) => {
   try {
     let config = {
       headers: {
-        'content-type': 'multipart/encrypted',
-        'x-auth-token': localStorage.getItem('x-auth-token')
-      }
+        "content-type": "multipart/encrypted",
+        "x-auth-token": localStorage.getItem("x-auth-token"),
+      },
     };
 
-
-    if(image !== '') {
+    if (image !== "") {
       try {
         await axios.delete(`api/images/delete-avatar/${email}/${image}`, {
           headers: {
-            'x-auth-token': localStorage.getItem('x-auth-token'),
-          }
+            "x-auth-token": localStorage.getItem("x-auth-token"),
+          },
         });
         dispatch({
-          type: IMAGE_DELETED
+          type: IMAGE_DELETED,
         });
       } catch (err) {
         console.log(err);
         dispatch({
-          type: IMAGE_DELETE_FAIL
+          type: IMAGE_DELETE_FAIL,
         });
         return;
       }
     }
 
     let body = new FormData();
-    body.append('file', file, file.name);
-    let res = await axios.post(`/api/images/upload/${email}/avatar`, body, config);
-    
+    body.append("file", file, file.name);
+    let res = await axios.post(
+      `/api/images/upload/${email}/avatar`,
+      body,
+      config
+    );
+
     dispatch({
       type: UPLOAD_IMAGE,
-      payload: res.data
+      payload: res.data,
     });
     return res.data;
-
   } catch (err) {
     console.log(err);
     dispatch({
-      type: UPLOAD_IMAGE_FAILED
+      type: UPLOAD_IMAGE_FAILED,
     });
   }
 };
 
-export const createProfile = (formData, token, email) => async dispatch => {
+export const createProfile = (formData, token, email) => async (dispatch) => {
   try {
     let config = {
       headers: {
-        'Content-Type': 'application/json',
-        'x-auth-token': token
-      }
+        "Content-Type": "application/json",
+        "x-auth-token": token,
+      },
     };
 
     formData = {
       ...formData,
-      email: email
+      email: email,
     };
 
-    let res = await axios.post('/api/profile/create-profile', formData, config);
-    console.log(res);
+    let res = await axios.post("/api/profile/create-profile", formData, config);
     dispatch({
       type: PROFILE_CREATED,
-      payload: res.data
+      payload: res.data,
     });
-
+    return res.status;
   } catch (err) {
     console.log(err);
     dispatch({
-      type: PROFILE_CREATE_ERROR
+      type: PROFILE_CREATE_ERROR,
     });
   }
 };
 
-export const editGameList = (gameList, email, token) => async dispatch => {
-
+export const editGameList = (gameList, email, token) => async (dispatch) => {
   try {
     let config = {
       headers: {
-        'Content-Type': 'application/json',
-        'x-auth-token': token
-      }
+        "Content-Type": "application/json",
+        "x-auth-token": token,
+      },
     };
-    let newGamlist = gameList.map(game => {
-      let {name, score, tags} = game;
-      return {name, score, tags};
+    let newGamlist = gameList.map((game) => {
+      let { name, score, tags } = game;
+      return { name, score, tags };
     });
 
-
-    let body = JSON.stringify({ 'gameList': newGamlist, email });
-    let res = await axios.post('api/games/add-game-to-profile', body, config);
+    let body = JSON.stringify({ gameList: newGamlist, email });
+    let res = await axios.post("api/games/add-game-to-profile", body, config);
 
     let existingGames = res.data.existingGames[0];
 
     config = {
       headers: {
-        'content-type': 'multipart/encrypted',
-        'x-auth-token': localStorage.getItem('x-auth-token')
-      }
+        "content-type": "multipart/encrypted",
+        "x-auth-token": localStorage.getItem("x-auth-token"),
+      },
     };
 
-
-    existingGames.map(async (game ,i) => {
-      
-      if(i === existingGames.length -1 && !game) {
+    existingGames.map(async (game, i) => {
+      if (i === existingGames.length - 1 && !game) {
         let config = {
           headers: {
-            'Content-Type': 'application/json',
-            'x-auth-token': token
-          }
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+          },
         };
         let res = await axios.get(`api/games/game-list/${email}`, config);
-          dispatch({
-            type: GAME_LIST_UPDATED,
-            payload: res.data
-          });
+        dispatch({
+          type: GAME_LIST_UPDATED,
+          payload: res.data,
+        });
       }
 
-      if(!game) return;
-      console.log(gameList[i].image , gameList[i].image.name);
+      if (!game) return;
+      console.log(gameList[i].image, gameList[i].image.name);
 
       body = new FormData();
-      body.append('file', gameList[i].image , gameList[i].image.name);
+      body.append("file", gameList[i].image, gameList[i].image.name);
 
-      await axios.post(`api/images/upload/${email}/Games:${gameList[i].name}`, body, config);
-      
-      if(i === existingGames.length -1) {
+      await axios.post(
+        `api/images/upload/${email}/Games:${gameList[i].name}`,
+        body,
+        config
+      );
+
+      if (i === existingGames.length - 1) {
         let config = {
           headers: {
-            'Content-Type': 'application/json',
-            'x-auth-token': token
-          }
+            "Content-Type": "application/json",
+            "x-auth-token": token,
+          },
         };
         let res = await axios.get(`api/games/game-list/${email}`, config);
-          dispatch({
-            type: GAME_LIST_UPDATED,
-            payload: res.data
-          });
+        dispatch({
+          type: GAME_LIST_UPDATED,
+          payload: res.data,
+        });
       }
     });
-
   } catch (err) {
     console.log(err);
   }
-} 
+};
