f98493ade93335b672a6f42f9660ef22887a344e
BAM-hub <bsharamin12@gmail.com>
2022-06-27 06:23:25 +0300

    on respose redirect to profile

--- client/src/components/profileForms/CreatePorfile.js
+++ client/src/components/profileForms/CreatePorfile.js
@@ -1,130 +1,147 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { createProfile } from '../../actions/profile';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { createProfile } from "../../actions/profile";
+import { Redirect } from "react-router-dom";
 
-
-const CreatePorfile = (
-  {auth:{email, token},
-  createProfile,
-  profile
-}) => {
-  
+const CreatePorfile = ({ auth: { email, token }, createProfile, profile }) => {
   const [formData, setFormData] = useState(profile);
+  const [redirect, setRedirect] = useState(false);
 
-  const {
-    staredGame,
-    social,
-    PreferedConsole
-  } = formData;
+  const { staredGame, social, PreferedConsole } = formData;
 
-  function onSubmit (e) {
+  async function onSubmit(e) {
     e.preventDefault();
-    createProfile(formData, token, email);
-  };
+    const status = await createProfile(formData, token, email);
+    if (status === 200) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Redirect to='/profile' />;
+  }
+
   return (
     <div className='profile-form'>
-      <form className='form' onSubmit={e => onSubmit(e)}>
-        <input 
-          type="text"
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <input
+          type='text'
           name='staredGame'
           value={staredGame.name}
-          onChange={e => setFormData({...formData,
-            staredGame:{
-              ...staredGame,
-              name: e.target.value
-            } })}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              staredGame: {
+                ...staredGame,
+                name: e.target.value,
+              },
+            })
+          }
           placeholder='Stared Game'
         />
-        <label htmlFor="score">Rate the game</label>
+        <label htmlFor='score'>Rate the game</label>
         <input
-          type="number"
+          type='number'
           name='score'
           value={staredGame.score}
-          onChange={e => setFormData({...formData,
-            staredGame:{
-              ...staredGame,
-              score: e.target.value
-            } })}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              staredGame: {
+                ...staredGame,
+                score: e.target.value,
+              },
+            })
+          }
         />
         <input
-          type="text"
+          type='text'
           name='tags'
           value={staredGame.tags}
-          onChange={e => setFormData({...formData,
-            staredGame:{
-              ...staredGame,
-              tags: e.target.value
-            } })}
-          placeholder='Genres'   
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              staredGame: {
+                ...staredGame,
+                tags: e.target.value,
+              },
+            })
+          }
+          placeholder='Genres'
         />
-        <input 
-          type="text"
+        <input
+          type='text'
           name='facebook'
           value={social.facebook}
-          onChange={e => setFormData({
-            ...formData,
-            social:{
-              ...social,
-              facebook: e.target.value
-            }
-          })}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              social: {
+                ...social,
+                facebook: e.target.value,
+              },
+            })
+          }
           placeholder='facebook'
         />
-        <input 
-          type="text"
+        <input
+          type='text'
           name='instagram'
           value={social.instagram}
-          onChange={e => setFormData({
-            ...formData,
-            social:{
-              ...social,
-              instagram: e.target.value
-            }
-          })}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              social: {
+                ...social,
+                instagram: e.target.value,
+              },
+            })
+          }
           placeholder='instagram'
         />
-        <input 
-          type="text"
+        <input
+          type='text'
           name='whatsapp'
           value={social.whatsapp}
-          onChange={e => setFormData({
-            ...formData,
-            social:{
-              ...social,
-              whatsapp: e.target.value
-            }
-          })}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              social: {
+                ...social,
+                whatsapp: e.target.value,
+              },
+            })
+          }
           placeholder='whatsapp'
         />
-        <input 
-          type="text" 
+        <input
+          type='text'
           name='preferdconsole'
           value={PreferedConsole}
-          onChange={e => setFormData({
-            ...formData,
-            PreferedConsole: e.target.value
-          })}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              PreferedConsole: e.target.value,
+            })
+          }
           placeholder='Prefered Console'
         />
-        <input type="submit" />
+        <input type='submit' />
       </form>
     </div>
   );
 };
 
-CreatePorfile.propTypes ={
+CreatePorfile.propTypes = {
   createProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  profile: state.profile
+  profile: state.profile,
 });
 
-export default connect(
-  mapStateToProps,
-  { createProfile }
-)(CreatePorfile);
+export default connect(mapStateToProps, { createProfile })(CreatePorfile);
