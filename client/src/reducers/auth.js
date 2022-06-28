763e93832aa05c0cb9eb5e95dfbe859f8eb3c3f6
BAM-hub <bsharamin12@gmail.com>
2022-06-29 01:37:32 +0300

    init socket in state

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,56 +5,59 @@ import {
   LOGIN_SUCCESS,
   LOGGEDIN,
   SOCKET_CONNECTION,
-  SOCKET_CLOSE
-} from '../actions/types';
+  SOCKET_CLOSE,
+  SOCKET_URL,
+} from "../actions/types";
 
-const initialState = { 
-  token: localStorage.getItem('token'),
+import { io } from "socket.io-client";
+
+const initialState = {
+  token: localStorage.getItem("token"),
   isAuthenticated: null,
   loading: true,
   user: null,
-  email: '',
-  name: '',
-  socket: null
+  email: "",
+  name: "",
+  socket: io(SOCKET_URL),
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   const { type, payload } = action;
-  switch(type) {
+  switch (type) {
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
-      localStorage.setItem('token', payload.token);
+      localStorage.setItem("token", payload.token);
       return {
         ...state,
         ...payload,
         isAuthenticated: true,
-        loading: false
+        loading: false,
       };
     case LOGGEDIN:
       return {
         ...state,
         ...payload,
         isAuthenticated: true,
-        loading: false
+        loading: false,
       };
     case REGISTER_FAIL:
     case LOGIN_FAIL:
-      localStorage.removeItem('tokent');
+      localStorage.removeItem("tokent");
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false,
-        socket: state.socket
+        socket: state.socket,
       };
     case SOCKET_CONNECTION:
     case SOCKET_CLOSE:
       return {
         ...state,
-        socket: payload
-      }
+        socket: payload,
+      };
     default:
       return state;
-  };
-};
+  }
+}
