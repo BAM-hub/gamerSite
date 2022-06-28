f1248036348e5ad8b3d43073e6b663c0d1d812ff
BAM-hub <bsharamin12@gmail.com>
2022-06-29 01:35:34 +0300

    formatted with prettier

--- socket/socket.js
+++ socket/socket.js
@@ -1,62 +1,60 @@
-const Chat = require('../models/Chat');
+const Chat = require("../models/Chat");
 
 const socketHandler = async (socket) => {
-    console.log(`connection by ${socket.id}`);
-    socket.on('join', (id) => {
-      socket.join(id);
-      console.log(`${id} joined`);
-   });
-  
-    socket.on('send_message', async msg => {
-      //console.log(msg);
-      const users = msg.users.map(user => {
-        if(user.email === msg.recipientEmail)
-          return {
-            name: user.name,
-            email: user.email,
-            alerts: {
-              count: ++user.alerts.count,
-              message: msg.message
-            }
-          };
-          return {
-            name: user.name,
-            email: user.email,
-            alerts: {
-              count: 0,
-              message: msg.message
-            }
-          }
-      });
-      await Chat.findOneAndUpdate(  
-        { conversationId: msg.chatId },
-        {
-          $addToSet: {
-            conversation: [
-              {
-                sender: msg.sender,
-                reciver: msg.reciver,
-                id: msg.id,
-                message: msg.message,
-                time: msg.time 
-              }
-            ]
+  console.log(`connection by ${socket.id}`);
+  socket.on("join", (id) => {
+    socket.join(id);
+    console.log(`${id} joined`);
+  });
+
+  socket.on("send_message", async (msg) => {
+    const users = msg.users.map((user) => {
+      if (user.email === msg.recipientEmail)
+        return {
+          name: user.name,
+          email: user.email,
+          alerts: {
+            count: ++user.alerts.count,
+            message: msg.message,
           },
-          users: users
-        });
-
-
-      socket.to(msg.reciver).emit('recive_message', msg);
+        };
+      return {
+        name: user.name,
+        email: user.email,
+        alerts: {
+          count: 0,
+          message: msg.message,
+        },
+      };
     });
+    await Chat.findOneAndUpdate(
+      { conversationId: msg.chatId },
+      {
+        $addToSet: {
+          conversation: [
+            {
+              sender: msg.sender,
+              reciver: msg.reciver,
+              id: msg.id,
+              message: msg.message,
+              time: msg.time,
+            },
+          ],
+        },
+        users: users,
+      }
+    );
 
-    socket.on('new-chat', (data) => {
-      socket.to(data.recipient).emit('new-chat', data);
-    });
+    socket.to(msg.reciver).emit("recive_message", msg);
+  });
 
-    socket.on('disconnect', () => {
-      console.log('disconnect');
-    });  
+  socket.on("new-chat", (data) => {
+    socket.to(data.recipient).emit("new-chat", data);
+  });
 
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+  });
 };
 
 module.exports = socketHandler;
