545317ae63f01a2497365ebd5e80a2f4321e2b89
BAM-hub <bsharamin12@gmail.com>
2022-06-29 01:38:07 +0300

    added the missing user field when creating a chat

--- client/src/reducers/chat.js
+++ client/src/reducers/chat.js
@@ -8,102 +8,107 @@ import {
   NEW_MESSAGE,
   CHAT_ALERTS_FOUND,
   CHAT_ALERTS_NOT_FOUND,
-  NEW_CHAT
-} from '../actions/types';
+  NEW_CHAT,
+} from "../actions/types";
 
 const initialState = {
   search: {
     _id: null,
-    name: '',
-    email: '',
-    date: ''
+    name: "",
+    email: "",
+    date: "",
   },
   selectedChat: {
-    _id: '',
+    _id: "",
   },
-  chats: []
+  chats: [],
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function(state = initialState, action) {
-  const  { type, payload } = action;
-  switch(type) {
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
     case USER_FOUND:
-      return { ...state, search: {...payload} };
+      return { ...state, search: { ...payload } };
     case SET_SELECTED_CHAT:
-      return { ...state, selectedChat: { _id: payload } }
+      return { ...state, selectedChat: { _id: payload } };
     case CONVERSATION_FOUND:
-      const updateChats = state.chats.map(chat => {
-        if(chat.conversationId === payload.conversationId)
+      const updateChats = state.chats.map((chat) => {
+        if (chat.conversationId === payload.conversationId)
           return {
             ...payload,
-            ...chat
-          }
+            ...chat,
+          };
         return chat;
       });
       return {
         ...state,
-        chats: updateChats
-      }
+        chats: updateChats,
+      };
     case CONVERSAITION_CREATED:
-      const { conversationId, recipientName, recipientEmail } = payload;
+      const { conversationId, recipientName, recipientEmail, users } = payload;
       return {
         ...state,
         chats: [
-          ...state.chats, {
-          conversationId: conversationId,
-          conversation: [],
-          alert: {
-            message: 'Empty',
-            count: 0
+          ...state.chats,
+          {
+            conversationId: conversationId,
+            conversation: [],
+            alert: {
+              message: "Empty",
+              count: 0,
+            },
+            recipientName,
+            recipientEmail,
+            users,
           },
-          recipientName,
-          recipientEmail
-        }]
-      }
+        ],
+      };
     case NEW_MESSAGE:
       const { id, msg, selectedChatId } = payload;
-      let newConversations = state.chats.map(convo => {
-
-        if(id === convo.conversationId && id === selectedChatId) {
+      let newConversations = state.chats.map((convo) => {
+        if (id === convo.conversationId && id === selectedChatId) {
           return {
             ...convo,
             conversation: [...convo.conversation, msg],
             alert: {
               message: msg.message,
-              count: 0
-            }
+              count: 0,
+            },
           };
         }
-        if(id === convo.conversationId) {
+        if (id === convo.conversationId) {
           return {
             ...convo,
             conversation: [...convo.conversation, msg],
             alert: {
               message: msg.message,
-              count: ++convo.alert.count
-            }
+              count: ++convo.alert.count,
+            },
           };
         }
         return convo;
       });
-      newConversations = newConversations.sort((a, b) => 
-       new Date(b.alert.message.time || new Date(1900,0,1)) - new Date(a.alert.message.time || new Date(1900,0,1)));
+      newConversations = newConversations.sort(
+        (a, b) =>
+          new Date(b.alert.message.time || new Date(1900, 0, 1)) -
+          new Date(a.alert.message.time || new Date(1900, 0, 1))
+      );
       return {
         ...state,
-        chats: newConversations
+        chats: newConversations,
       };
     case USER_NOT_FOUND:
     case CLEAR_SEARCH:
-        return {
-          ...state, 
-          search: {
-            _id: null,
-            name: '',
-            email: '',
-            date: ''
-          }
-        };
+      return {
+        ...state,
+        search: {
+          _id: null,
+          name: "",
+          email: "",
+          date: "",
+        },
+      };
     case NEW_CHAT:
       const { convoId, myName, myEmail } = payload;
       return {
@@ -113,20 +118,20 @@ export default function(state = initialState, action) {
           {
             alert: {
               count: 0,
-              message: 'Empty'
+              message: "Empty",
             },
             conversationId: convoId,
             recipientEmail: myEmail,
-            recipientName: myName
-          }
-        ]
-      }    
-    case CHAT_ALERTS_FOUND: 
-      return { 
+            recipientName: myName,
+          },
+        ],
+      };
+    case CHAT_ALERTS_FOUND:
+      return {
         ...state,
-        chats: payload
-      }
-    case CHAT_ALERTS_NOT_FOUND:  
+        chats: payload,
+      };
+    case CHAT_ALERTS_NOT_FOUND:
     default:
       return state;
   }
