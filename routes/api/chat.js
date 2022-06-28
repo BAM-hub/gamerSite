2f5d8c0c635ea6bcab599f73059fa61c49bb66f5
BAM-hub <bsharamin12@gmail.com>
2022-06-29 00:46:39 +0300

    fixed abug where if no chats are available server crashes

--- routes/api/chat.js
+++ routes/api/chat.js
@@ -1,128 +1,129 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const jwt = require('jsonWebToken');
+const auth = require("../../middleware/auth");
+const jwt = require("jsonWebToken");
 
 // TBD
-const Chat = require('../../models/Chat');
-const Profile = require('../../models/Profile.js');
-const User = require('../../models/User');
+const Chat = require("../../models/Chat");
+const Profile = require("../../models/Profile.js");
+const User = require("../../models/User");
 
 // POST Register new conversations
 // private /api/chat
 
-router.post('/create-conversation', auth, async (req, res) => {
-  
-  const { creator, recipient } = req.body; 
+router.post("/create-conversation", auth, async (req, res) => {
+  const { creator, recipient } = req.body;
   try {
-
     let creatorData = await User.findOne({ email: creator });
-    let recipientData =  await User.findOne({ email: recipient });
+    let recipientData = await User.findOne({ email: recipient });
 
-    if(!recipientData){
-      return res.send('recipient not found');
+    if (!recipientData) {
+      return res.send("recipient not found");
     }
 
-    const oldConvo = await Chat.findOne({ conversationId: creatorData.id + ' ' + recipientData.id });
-    if(oldConvo){
-      return res.send('there already is a conversation between you');
+    const oldConvo = await Chat.findOne({
+      conversationId: creatorData.id + " " + recipientData.id,
+    });
+    if (oldConvo) {
+      return res.send("there already is a conversation between you");
     }
 
     const conversation = new Chat({
-      conversationId: creatorData.id + ' ' + recipientData.id,
+      conversationId: creatorData.id + " " + recipientData.id,
       users: [
         { email: creatorData.email, name: creatorData.name },
-        { email: recipientData.email, name: recipientData.name }
-      ]
+        { email: recipientData.email, name: recipientData.name },
+      ],
     });
 
     await conversation.save();
 
     const creatorProfile = await Profile.findOne({ email: creator });
     const recipientProfile = await Profile.findOne({ email: recipient });
-    
+
     let creatorConversations = creatorProfile.conversations;
     let recipientConversations = recipientProfile.conversations;
 
-
     creatorConversations.push({
-      conversationId: creatorData.id + ' ' + recipientData.id,
+      conversationId: creatorData.id + " " + recipientData.id,
       recipientName: recipientData.name,
       recipientEmail: recipient,
-      image: recipientProfile.image
+      image: recipientProfile.image,
     });
     await Profile.findOneAndUpdate(
       { email: creator },
-      { conversations: creatorConversations });
-
+      { conversations: creatorConversations }
+    );
 
     recipientConversations.push({
-      conversationId: creatorData.id + ' ' + recipientData.id,
+      conversationId: creatorData.id + " " + recipientData.id,
       recipientName: creatorData.name,
       recipientEmail: creator,
-      image: creatorProfile.image
+      image: creatorProfile.image,
     });
 
     await Profile.findOneAndUpdate(
       { email: recipient },
-      { conversations: recipientConversations });
+      { conversations: recipientConversations }
+    );
 
     res.send(conversation);
-    
   } catch (err) {
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
-  
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     //use find for potential documents spliting
     const chat = await Chat.find({ conversationId: req.params.id })
-      .sort({ dateCreated: -1 }).limit(1);
+      .sort({ dateCreated: -1 })
+      .limit(1);
     res.send(chat[0]);
   } catch (err) {
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
 //private route
 //get user by email
-router.get('/get-user-by-email/:email', async (req, res) => {
+router.get("/get-user-by-email/:email", async (req, res) => {
   try {
     const { email } = req.params;
-    const user = await User.findOne({ email: email }).select('-password');
-    if(!user) return res.send('User Not Found!');
+    const user = await User.findOne({ email: email }).select("-password");
+    if (!user) return res.send("User Not Found!");
     res.send(user);
   } catch (err) {
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
 });
 
-router.get('/get-user-alerts/:id/:email', async (req,res) => {
+router.get("/get-user-alerts/:id/:email", async (req, res) => {
   const { id, email } = req.params;
   try {
     const profile = await Profile.findById(id);
     const { conversations } = profile;
-    
-    const alerts = await Promise.all(conversations.map( async convo => {
+
+    const alerts = await Promise.all(
+      conversations.map(async (convo) => {
         try {
-          const chat = await Chat.findOne({ conversationId: convo.conversationId });
-          const { users, conversationId } = chat;
-          const alert =  users.filter(user => {
-            if(user.email === email) 
-              return {alerts: user.alerts};
+          const chat = await Chat.findOne({
+            conversationId: convo.conversationId,
           });
-          console.log(alert);
-          return {alert: alert[0], conversationId};
+          if (!chat) return null;
+          const { users, conversationId } = chat;
+          const alert = users.filter((user) => {
+            if (user.email === email) return { alerts: user.alerts };
+          });
+          return { alert: alert[0], conversationId };
         } catch (err) {
-          res.status(500).send('Internal Server Error');
+          res.status(500).send("Internal Server Error");
         }
       })
     );
     return res.send(alerts);
   } catch (err) {
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
 });
 
