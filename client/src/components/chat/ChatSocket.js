eb209dd84f9cbafb26f043d0b94f29d42076d7e2
BAM-hub <bsharamin12@gmail.com>
2022-06-29 01:37:14 +0300

    moved out  the socket connection from here for a stable connection

--- client/src/components/chat/ChatSocket.js
+++ client/src/components/chat/ChatSocket.js
@@ -1,103 +1,80 @@
 import { useEffect, Fragment, useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { useDispatch, connect } from "react-redux";
 import { useLocation } from "react-router-dom";
-import { io } from "socket.io-client";
-import { NEW_CHAT, SOCKET_CLOSE, SOCKET_CONNECTION, SOCKET_URL } from "../../actions/types";
+import { NEW_CHAT } from "../../actions/types";
 import Chat from "./Chat";
 import ChatOverview from "./ChatOverview";
 import { newMessage, findChat, getUserAlerts } from "../../actions/chat";
 
-const ChatSocket = ({ 
-  user,
+const ChatSocket = ({
   socket,
-  newMessage, 
+  newMessage,
   findChat,
   getUserAlerts,
   profile: { _id, email, conversations },
-  chat: {selectedChat, chats} 
+  chat: { selectedChat, chats },
 }) => {
   const dispatch = useDispatch();
   const location = useLocation();
   const [fetchedChats, setfetchedChats] = useState([]);
 
   useEffect(() => {
-    if(chats.length === 0)
-    getUserAlerts(_id, email, conversations);
+    if (chats.length === 0) getUserAlerts(_id, email, conversations);
   }, [_id, email, conversations, getUserAlerts, chats.length]);
 
   useEffect(() => {
-    if(fetchedChats.includes(selectedChat._id)) return;
+    if (fetchedChats.includes(selectedChat._id)) return;
     findChat(selectedChat._id);
-    setfetchedChats(prevState => [...prevState, selectedChat._id]);
+    setfetchedChats((prevState) => [...prevState, selectedChat._id]);
   }, [fetchedChats, findChat, selectedChat._id]);
 
   useEffect(() => {
-    const socket = io(SOCKET_URL);
-    dispatch({
-      type: SOCKET_CONNECTION,
-      payload: socket
-    });
-    socket.emit('join', user);
-    return () => {
-      socket.off('join');
-      socket.off('connection');
-      dispatch({
-        type: SOCKET_CLOSE,
-        payload: io(SOCKET_URL)
-      });
-      socket.close();
-    }
-  }, [dispatch, user]);
-
-  useEffect(() => {
-    socket?.on('recive_message', (msg) => {
-      newMessage(msg.chatId,selectedChat._id , msg, chats);
+    socket?.on("recive_message", (msg) => {
+      newMessage(msg.chatId, selectedChat._id, msg, chats);
     });
     return () => {
-      socket?.off('recive_message');
-    }
+      socket?.off("recive_message");
+    };
   });
 
   useEffect(() => {
-    socket?.on('new-chat', data => { 
+    socket?.on("new-chat", (data) => {
       dispatch({
         type: NEW_CHAT,
-        payload: data
+        payload: data,
       });
-      findChat(data.convoId)
+      findChat(data.convoId);
     });
     return () => {
-      socket?.off('new-chat');
-    }
+      socket?.off("new-chat");
+    };
   });
 
   return (
     <Fragment>
-      {location.pathname === '/profile' ? <ChatOverview /> : <Chat />}
+      {location.pathname === "/profile" ? <ChatOverview /> : <Chat />}
     </Fragment>
-  )
-}
+  );
+};
 
 ChatSocket.propTypes = {
-  user: PropTypes.string.isRequired,
   socket: PropTypes.object,
   newMessage: PropTypes.func.isRequired,
   chat: PropTypes.object.isRequired,
   findChat: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
-  getUserAlerts: PropTypes.func.isRequired
-}
+  getUserAlerts: PropTypes.func.isRequired,
+};
 
-const mapStateToProps = state => ({
-  user: state.auth.user,
+const mapStateToProps = (state) => ({
   profile: state.profile,
   socket: state.auth.socket,
-  chat: state.chat
+  chat: state.chat,
 });
 
-
-export default connect(
-  mapStateToProps,
-  { newMessage, findChat, getUserAlerts }
-  )(ChatSocket);
+export default connect(mapStateToProps, {
+  newMessage,
+  findChat,
+  getUserAlerts,
+})(ChatSocket);
