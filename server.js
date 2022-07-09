f2d20d011694f3cd6656849d2bcccf21c5c38fa6
BAM-hub <bsharamin12@gmail.com>
2022-07-09 11:01:46 +0300

    deploy

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const connectDB = require('./config/db');
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
-const socketHandler = require('./socket/socket');
+const socketHandler = require("./socket/socket");
 
 const httpServer = createServer(app);
 
-
 const io = new Server(httpServer, {
-cors: {
+  cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     //allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
-io.on('connection', socket => {socketHandler(socket)});
-
+io.on("connection", (socket) => {
+  socketHandler(socket);
+});
 
 //connect db
 connectDB();
@@ -29,20 +30,28 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-
-app.get('/', (req, res) => res.send('API running'));
+app.use(methodOverride("_method"));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/games', require('./routes/api/games'));
-app.use('/api/chat', require('./routes/api/chat'));
-app.use('/api/images', require('./routes/api/images'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/games", require("./routes/api/games"));
+app.use("/api/chat", require("./routes/api/chat"));
+app.use("/api/images", require("./routes/api/images"));
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
-httpServer.listen(PORT, () => { 
-    console.log(`server started on ${PORT}`);
+httpServer.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
 });
