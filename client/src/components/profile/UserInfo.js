839fe6218961b9cdedfc71ab34291ce9610b9a50
BAM-hub <bsharamin12@gmail.com>
2022-07-09 18:28:49 +0300

    not using the cdn anymore

--- client/src/components/profile/UserInfo.js
+++ client/src/components/profile/UserInfo.js
@@ -1,112 +1,111 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import logo from '../layout/avatarph.jpg';
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import logo from "../layout/avatarph.jpg";
+import { Redirect } from "react-router-dom";
+import { IMAGE_PREFIX } from "../../actions/types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faInstagramSquare,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPollH } from "@fortawesome/free-solid-svg-icons";
 
-
-const UserInfo = ({
-  name,
-  profile:{
-    social,
-    staredGame,
-    image
-  },
-}) => {
-
+const UserInfo = ({ name, profile: { social, staredGame, image } }) => {
   const [uploadImage, setuploadImage] = useState(false);
-  if(uploadImage) {
-    return <Redirect to='/upload-image' />
+  if (uploadImage) {
+    return <Redirect to='/upload-image' />;
   }
 
   return (
-    <div className="left">
-
-    <div className="info">
-      <div className="avatar" onClick={
-        ()=> setuploadImage(true)}>
-      {
-        image === '' ? 
-        <img src={logo} alt='avatar' /> 
-        :
-        <img src={`http://localhost:5000/api/images/avatar/${image}`} alt="avatar"/>
-      }
-
+    <div className='left'>
+      <div className='info'>
+        <div className='avatar' onClick={() => setuploadImage(true)}>
+          {image === "" ? (
+            <img src={logo} alt='avatar' />
+          ) : (
+            <img src={`${IMAGE_PREFIX}/avatar/${image}`} alt='avatar' />
+          )}
+        </div>
+        <div className='name'>
+          <p>{name}</p>
+        </div>
       </div>
-      <div className="name">
-        <p>{name}</p>
+
+      {social === undefined ? (
+        <div className='social'>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faPollH} />
+              {/* <i className='fas fa-poll-h'></i> */}
+              <span>Nothing to show here.</span>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div className='social'>
+          <ul>
+            {social.facebook !== "" && (
+              <li>
+                <FontAwesomeIcon icon={faFacebookSquare} />
+                {/* <i className='fab fa-facebook-square'></i> */}
+                <span>{social.facebook}</span>
+              </li>
+            )}
+            {social.instagram !== "" && (
+              <li>
+                {/* <i className='fab fa-instagram'></i> */}
+                <FontAwesomeIcon icon={faInstagramSquare} />
+                <span>{social.instagram}</span>
+              </li>
+            )}
+            {social.whatsapp !== "" && (
+              <li>
+                <FontAwesomeIcon icon={faWhatsapp} />
+                {/* <i className='fab fa-whatsapp'></i> */}
+                <span>{social.whatsapp}</span>
+              </li>
+            )}
+            {social.facebook === "" &&
+              social.instagram === "" &&
+              social.whatsapp === "" && (
+                <li>
+                  <i className='fas fa-poll-h'></i>
+                  <span>Nothing to show here.</span>
+                </li>
+              )}
+          </ul>
+        </div>
+      )}
+
+      <div className='stared-game'>
+        <div className='game-pic'>
+          <img
+            src='https://www.zelda.com/breath-of-the-wild/assets/icons/BOTW-Share_icon.jpg'
+            alt='game'
+          />
+        </div>
+        <div className='game-name'>
+          <p>{staredGame.name}</p>
+        </div>
+        <div className='score'>
+          <p>{staredGame.score}</p>
+        </div>
+        <div className='tags'>{staredGame.tags}</div>
       </div>
     </div>
-    
-    {social === undefined ?
-    <div className="social">
-      <ul>
-        <li>
-          <i className="fas fa-poll-h"></i><span>Nothing to show here.</span>
-        </li>
-      </ul> 
-    </div>
-    :
-    <div className="social">
-      <ul>
-        {
-          social.facebook !== '' && 
-        <li>
-          <i className="fab fa-facebook-square"></i><span>{social.facebook}</span>
-        </li>
-        }
-        {
-        social.instagram !== ''&&
-        <li>
-            <i className="fab fa-instagram"></i><span>{social.instagram}</span>
-        </li>
-        }
-        {
-          social.whatsapp !== '' &&
-        
-        <li>
-            <i className="fab fa-whatsapp"></i><span>{social.whatsapp}</span>
-        </li>
-        }
-        {
-          social.facebook === '' && social.instagram === ''&& social.whatsapp === '' &&
-          <li>
-            <i className="fas fa-poll-h"></i><span>Nothing to show here.</span>
-          </li>
-        }
-      </ul>
-    </div>
-    }
-
-    <div className="stared-game">
-      <div className="game-pic">
-        <img src='https://www.zelda.com/breath-of-the-wild/assets/icons/BOTW-Share_icon.jpg' alt="game" />
-      </div>
-      <div className="game-name">
-        <p>{staredGame.name}</p>
-      </div>
-      <div className="score">
-        <p>{staredGame.score}</p>
-      </div>
-      <div className="tags">
-        {staredGame.tags}
-      </div>
-    </div>
-
-  </div>
   );
 };
 
 UserInfo.propTypes = {
   profile: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   profile: state.profile,
-  name: state.auth.name
+  name: state.auth.name,
 });
 
-export default connect(
-  mapStateToProps
-)(UserInfo);
+export default connect(mapStateToProps)(UserInfo);
