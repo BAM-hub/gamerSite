773fe9e6509fcd028a8c18bb6dcb06de48f95fc6
BAM-hub <bsharamin12@gmail.com>
2022-07-15 15:37:11 +0300

    filled missing auth data needed

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,93 +5,100 @@ import {
   REGISTER_FAIL,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGGEDIN
-} from './types';
+  LOGGEDIN,
+} from "./types";
 
-// register user 
-export const register = ({ name, email, password }) => async dispatch => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }
-  const body = JSON.stringify({ name, email, password });
-
-  try {
-    const res = await axios.post('/api/users', body, config);
-
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data
-    });
-  } catch (err) {
-    const errors = err.response.data.errors;
-
-    if(errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+// register user
+export const register =
+  ({ name, email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
+    const body = JSON.stringify({ name, email, password });
 
-    dispatch({
-      type: REGISTER_FAIL
-    });
-  }
-};
+    try {
+      const res = await axios.post("/api/users", body, config);
 
-export const login = ({ email, password }) => async dispatch => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json'
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: {
+          token: res.data.token,
+          email,
+          name,
+        },
+      });
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: errors,
+      });
     }
   };
 
-  const body = JSON.stringify({ email, password });
-
-  try {
-    const res = await axios.post('api/auth', body, config);
-    
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data
-    });
-    localStorage.setItem('x-auth-token', res.data.token);
-  } catch (err) {
-    const errors = err.response.data.errors;
-
-    if(errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+export const login =
+  ({ email, password }) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    dispatch({
-      type: LOGIN_FAIL
-    });
-  }
-}
+    const body = JSON.stringify({ email, password });
 
-export const loggedIn = () => async dispatch => {
-  let token = localStorage.getItem('token');
-  const config = {
-    headers: {
-      'x-auth-token': token
+    try {
+      const res = await axios.post("api/auth", body, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+      localStorage.setItem("x-auth-token", res.data.token);
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+
+      dispatch({
+        type: LOGIN_FAIL,
+      });
     }
   };
 
-  try {
+export const loggedIn = () => async (dispatch) => {
+  let token = localStorage.getItem("token");
+  const config = {
+    headers: {
+      "x-auth-token": token,
+    },
+  };
 
-    let res = await axios.get('api/auth', config);
+  try {
+    let res = await axios.get("api/auth", config);
     dispatch({
       type: LOGGEDIN,
-      payload: res.data
+      payload: res.data,
     });
     console.log(res.data);
     return true;
-
   } catch (err) {
     const errors = err.response.data.errors;
 
-    if(errors) {
-      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
-    };
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
 
     return false;
   }
-}
+};
